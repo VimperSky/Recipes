fdfc756ee1a76043ae8592c9da809b1f6791b336
ImperialMan <1>
2021-07-07 20:08:38 +0300

    test-recipes-manager added constant for images path

--- recipes-angular/src/app/core/services/test-recipes-manager.service.ts
+++ recipes-angular/src/app/core/services/test-recipes-manager.service.ts
@@ -2,9 +2,11 @@ import { Injectable } from '@angular/core';
 import {Recipe} from "../models/recipe";
 import {RecipesManagerService} from "./recipes-manager.service";
 
+const testImagesPath =  "../../../assets/images/test/";
+
 export const recipes: Recipe[] = [
   {
-    imagePath: "../../../assets/images/r1.png",
+    imagePath: testImagesPath + "r1.png",
     header: "Клубичная Панна-Котта",
     description: "Десерт, который невероятно легко и быстро готовится. " +
       "Советую подавать его порционно в красивых бокалах, украсив взбитыми сливками, свежими ягодами и мятой.",
@@ -12,14 +14,14 @@ export const recipes: Recipe[] = [
     portions: 5
   },
   {
-    imagePath: "../../../assets/images/r2.png",
+    imagePath: testImagesPath + "r2.png",
     header: "Мясные фрикадельки",
     description: "Мясные фрикадельки в томатном соусе - несложное и вкусное блюдо, которым можно порадовать своих близких. ",
     cookingTime: 90,
     portions: 4
   },
   {
-    imagePath: "../../../assets/images/r3.png",
+    imagePath: testImagesPath + "r3.png",
     header: "Панкейки",
     description: "Панкейки: меньше, чем блины, но больше, чем оладьи. Основное отличие — в тесте, " +
       "оно должно быть воздушным, чтобы панкейки не растекались по сковородке...",
@@ -27,7 +29,7 @@ export const recipes: Recipe[] = [
     portions: 3
   },
   {
-    imagePath: "../../../assets/images/r4.png",
+    imagePath: testImagesPath + "r4.png",
     header: "Полезное мороженое без сахара",
     description: "Йогуртовое мороженое сочетает в себе нежный вкус и низкую калорийность, " +
       "что будет особенно актуально для сладкоежек, соблюдающих диету.",
@@ -35,7 +37,7 @@ export const recipes: Recipe[] = [
     portions: 2
   },
   {
-    imagePath: "../../../assets/images/r5.jpg",
+    imagePath: testImagesPath + "r5.jpg",
     header: "БИСКВИТ КЛАССИЧЕСКИЙ",
     description: "Сегодня хочу показать, как приготовить идеально пышный бисквит для торта или пирожных." +
       " Очень простой рецепт без использования разрыхлителя и соды. Только мука, яйца, сахар и небольшая щепотка соли. Именно такие ингредиенты используются для классического пышного бисквита",
