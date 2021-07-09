483d3b969479facfc54526f6c402afcaf5b1cdc6
ImperialMan <1>
2021-07-09 17:41:54 +0300

    fixes, added eslint config

--- recipes-angular/src/app/embedded/recipe-search/recipe-search.component.ts
+++ recipes-angular/src/app/embedded/recipe-search/recipe-search.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
 import {RecipesManagerService} from "../../core/services/recipes-manager.service";
 
